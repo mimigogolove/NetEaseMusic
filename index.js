4c6b2b5ac03306361db8fa543d82dbb1d51a0e6c
mimigogolove <953749837@qq.com>
2018-01-14 22:35:31 +0800

    切换

--- index.js
+++ index.js
@@ -25,6 +25,85 @@ $(function(){
 
 
 
-	        
+	$('.siteNav').on('click', 'ol.tabItems>li',function(e){
+		let $li = $(e.currentTarget).addClass('active')
+		$li.siblings().removeClass('active')
+		let index = $li.index()
+		$li.trigger('tabChange',index)
+		$('.tabContent > li').eq(index).addClass('active')
+		.siblings().removeClass('active')
+	})
+
+     $('.siteNav').on('tabChange',function(e, index){
+     	let $li = $('.tabContent > li').eq(index)
+     	if($li.attr('data-downloaded') ==='yes'){
+     		return
+     		}
+        if(index === 1){
+     		$.get('./page2.json').then((response) => {
+     			$li.text(response.content)
+     			$li.attr('data-downloaded','yes')
+
+
+     		})
+     	}else if(index === 2){
+     		return
+     		$.get('./page3.json').then((response) => {
+     			$li.text(response.content)
+     			$li.attr('data-downloaded','yes')
+
+     		})
+
+     	}
+     })
+
+
+let timer = undefined
+
+$('input#searchSong').on('input',function(e){
+  let $input = $(e.currentTarget)
+  let value = $input.val().trim()
+  if(value === ''){return}
+  if(timer){
+    clearTimerout(timer)
+    }
+    
+
+  timer = setTimeout(function(){
+ 	   search(value).then((result)=>{
+ 	   	timer = undefined
+ 		if(result.length !== 0){
+ 			$('#output').text(result.map((r)=>r.name).join(','))
+ 		}else{
+ 			$('#output').text('没有结果')
+ 		
+ 		}
+ 	})
+
+ 	},300)
+    
+
+
+ 	
+ })    
 	
+
+function search(keyword){
+	return new Promise((resolve,reject)=>{
+		var database = [
+  {"id": 1,"name" : "说散就散"},
+  {"id": 2,"name": "追光者"},
+  {"id": 3,"name": "远走高飞"},
+  {"id": 4,"name": "暧昧"}
+	]
+	let result = database.filter(function(item){
+		return item.name.indexOf(keyword)>=0
+
+	})
+	setTimeout(function(){
+		resolve(result)
+		},(Math.random()*200+1000))
+	
+	})
+}
 })
